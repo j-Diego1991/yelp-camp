f4940615f73e11e7abbbe168c536abb3444fd31a
j-Diego1991 <79339323+j-Diego1991@users.noreply.github.com>
2023-06-30 22:34:09 -0600

    Added Validation methods
    Added validation methods using the module Joi to check server-side data validations and bootstrap to check client-side data validations
    
    Also, it was created the directory utils to save the methods and functions to catch Async errors in the app.js file

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 const express = require('express')
 const path = require('path')
+const morgan = require('morgan')
 const mongoose = require('mongoose')
 const ejsMate = require('ejs-mate')
+const { campgroundSchema } = require('./schemas.js')
+const catchAsync = require('./utils/catchAsync')
+const ExpressError = require('./utils/ExpressError')
 const methodOverride = require('method-override')
 const Campground = require('./models/campground')
 
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp')
-.then(()=>{
-    console.log('DATABASE CONNECTED')
-})
-.catch(()=>{
-    console.log('CONEXION ERROR')
-    console.log(err)
-})
+    .then(() => {
+        console.log('DATABASE CONNECTED')
+    })
+    .catch(() => {
+        console.log('CONEXION ERROR')
+        console.log(err)
+    })
 
 const app = express()
 
@@ -21,8 +25,21 @@ app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
+app.use(morgan('dev'))
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+
+
+const validateCampground = (req, res, next) => {
+    const { error } = campgroundSchema.validate(req.body)
+    if (error) {
+        const msg = error.details.map( el => el.message).join(',')
+        throw new ExpressError(msg, 400)
+    } else {
+        next()
+    }
+}
+
 
 app.get('/', (req, res) => {
     res.render('home')
@@ -33,40 +50,46 @@ app.get('/campgrounds', async (req, res) => {
     res.render('campgrounds/index', { campgrounds })
 })
 
-/* La ruta para crear un nuevo elemento
-   no lleva caracteristicas asincronas */
-   app.get('/campgrounds/new', (req, res) => {
+
+app.get('/campgrounds/new', (req, res) => {
     res.render('campgrounds/new')
 })
-app.post('/campgrounds', async (req, res) => {
+app.post('/campgrounds', validateCampground, catchAsync(async (req, res, next) => {
     const campground = new Campground(req.body.campground)
     await campground.save()
     res.redirect(`/campgrounds/${campground.id}`)
-})
+}))
 
-app.get('/campgrounds/:id', async (req, res) => {
+app.get('/campgrounds/:id', catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id)
     res.render('campgrounds/show', { campground })
-})
+}))
 
-app.get('/campgrounds/:id/edit', async (req, res) => {
+app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id)
     res.render('campgrounds/edit', { campground })
-})
-app.put('/campgrounds/:id', async (req, res) => {
-    const {id} = req.params
-    const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground})
+}))
+app.put('/campgrounds/:id', validateCampground, catchAsync(async (req, res) => {
+    const { id } = req.params
+    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground })
     res.redirect(`/campgrounds/${campground.id}`)
-})
+}))
 
-app.delete('/campgrounds/:id', async(req, res) => {
-    const {id} = req.params
+app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
+    const { id } = req.params
     await Campground.findByIdAndDelete(id)
     res.redirect('/campgrounds')
+}))
+
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found', 404))
 })
 
-
-
+app.use((err, req, res, next) => {
+    const {statusCode = 500 } = err
+    if(!err.message) err.message = 'Something went wrong!'
+    res.status(statusCode).render('error', { err })
+})
 app.listen(3000, () => {
     console.log('Serving on port 3000')
 })
