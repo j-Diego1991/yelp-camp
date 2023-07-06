bac8130a600e14c0d46f2acbe324ef6fe4c5e23f
j-Diego1991 <79339323+j-Diego1991@users.noreply.github.com>
2023-07-06 14:09:37 -0600

    Moved routes and cleaning code
    Move the app routes, for both models user and reviews, to separate the logic and functions into a different file.
    Created a five-star range input, with starability, to replace the default range input on the reviews' view
    And other security fixes

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,31 +1,15 @@
 const express = require('express')
 const router = express.Router({ mergeParams: true })
 const catchAsync = require('../utils/catchAsync')
-const Campground = require('../models/campground')
-const Review = require('../models/review')
+const reviews = require('../controllers/reviews')
 const { validateRewiew, isLoggedIn, isReviewAuthor } = require('../middleware')
 
 
 
-// rutas para las reviews
-router.post('/', isLoggedIn, validateRewiew, catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id)
-    const review = new Review(req.body.review)
-    review.author = req.user.id
-    campground.reviews.push(review)
-    await review.save()
-    await campground.save()
-    req.flash('success', 'Successfully added review')
-    res.redirect(`/campgrounds/${campground.id}`)
-}))
 
-router.delete('/:reviewId', isLoggedIn, isReviewAuthor, catchAsync( async (req, res) => {
-    const { id, reviewId } = req.params
-    await Campground.findByIdAndUpdate(id, { $pull: {reviews: reviewId } })
-    await Review.findByIdAndDelete(reviewId)
-    req.flash('success', 'Successfully deleted review')
-    res.redirect(`/campgrounds/${id}`)
-}))
-//
+router.post('/', isLoggedIn, validateRewiew, catchAsync(reviews.createReview))
+
+router.delete('/:reviewId', isLoggedIn, isReviewAuthor, catchAsync(reviews.deleteReview))
+
 
 module.exports = router
